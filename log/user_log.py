5a08b9d7e2e9c84010113539951517fe0054b15a
huanshenyi <txy1226052@gmail.com>
2019-06-18 08:06:55 +0900

    log出力テスト完成

--- log/user_log.py
+++ log/user_log.py
@@ -8,10 +8,14 @@ logger.setLevel(logging.DEBUG)
 
 #logをファイルに出力
 file_handle = logging.FileHandler(r"D:\program\automation_test\automation\log\logs\test.log")
+# log出力のフォーマット
+formatter = logging.Formatter('%(asctime)s %(filename)s--> %(funcName)s %(levelno)s: %(levelname)s --->%(message)s')
+# フォーマット使用
+file_handle.setFormatter(formatter)
 logger.addHandler(file_handle)
 
 # terminalで出力
-logger.debug("teste")
+logger.debug("teste1234")
 # 終了後閉じる
 file_handle.close()
 logger.removeHandler(file_handle)
